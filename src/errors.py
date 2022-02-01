0b7cd5a2447d30f319200ac3b8a903ab9c9bedfb
Joshua Lochner <admin@xenova.com>
2022-02-01 22:06:36 +0200

    Update errors

--- src/errors.py
+++ src/errors.py
@@ -4,10 +4,20 @@ class SponsorBlockException(Exception):
 
 
 class PredictionException(SponsorBlockException):
-    """An exception was occurred while predicting sponsor segments"""
+    """An exception occurred while predicting sponsor segments"""
     pass
 
 
 class TranscriptError(SponsorBlockException):
-    """An exception was occurred while retrieving the video transcript"""
+    """An exception occurred while retrieving the video transcript"""
+    pass
+
+
+class ModelError(SponsorBlockException):
+    """Base class for model-related errors"""
+    pass
+
+
+class ModelLoadError(ModelError):
+    """An exception occurred while loading the model"""
     pass
